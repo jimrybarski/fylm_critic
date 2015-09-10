d6fb6bb47c55c81520e9b6824e75027667324749
jim <jim@rybarski.com>
2015-09-10 11:51:59 -0500

    #7 added comments and skeletons for rotation/translation

--- model/image_stack.py
+++ model/image_stack.py
@@ -7,26 +7,38 @@ class ImageStack(object):
     def __init__(self):
         self._image_sets = {}
         self._image_lookup = {}
+        self._translations = {}
+        self._rotations = {}
 
-    def add(self, image_set):
+    def add(self, image_set, translations=None, rotations=None):
         """ Adds a set of images to the virtual image stack. """
         # ensure the image set is valid
         if not (hasattr(image_set, "__getitem__") and hasattr(image_set, "__len__")):
             raise TypeError("You tried to add an image set to the ImageStack but it doesn't provide the right interface (__getitem__ and __len__)")
-        # the index is the range of frame numbers
+        # we need a new slot in our _image_sets dictionary for the new image set we've just been given
+        # since we want the last index plus one, we can just use the length of _image_sets
         image_set_index = len(self._image_sets)
+        # we need to know how many total images we already have, so when we index the new images in the new image set,
+        # we can start from the next available index
         image_count = len(self._image_lookup)
         for i in range(len(image_set)):
+            # for each image, we create a new global index number, and map that to the particular image set it came from and its local index number
             self._image_lookup[image_count + i] = (image_set_index, i)
+        # here we just store the image set so we can access the images from it
         self._image_sets[image_set_index] = image_set
+        self._translations[image_set_index] = translations
+        self._rotations[image_set_index] = rotations
 
-    def __len__(self):
+    def __len__(self) -> int:
+        """ The number of total images there are in all the image sets. """
         return len(self._image_lookup)
 
-    def __getitem__(self, item):
-        if not isinstance(item, int):
+    def __getitem__(self, index):
+        if not isinstance(index, int):
             raise TypeError("ImageStack uses integer indexes only.")
-        if item < 0 or item >= len(self):
+        if index < 0 or index >= len(self):
             raise ValueError("Out of bounds index access for ImageStack")
-        image_set_index, image_index = self._image_lookup[item]
+        # We have several sets of images, each indexed from 0 to some arbitrary number. We need to figure out which image set the given index maps to
+        # Once we know the image set, we also need to know which image we should use
+        image_set_index, image_index = self._image_lookup[index]
         return self._image_sets[image_set_index][image_index]
--- tests/model/image_stack.py
+++ tests/model/image_stack.py
@@ -22,6 +22,11 @@ class ImageStackTests(unittest.TestCase):
         self.assertListEqual(sorted(list(stack._image_lookup.keys())), [0, 1, 2, 3, 4])
 
     def test_getitem(self):
+        """
+        We combine several sets of images, each of which starts its own index at zero.
+        We want to be able to combine them into one superset and have a single range of indices.
+
+        """
         stack = ImageStack()
         image_set = {0: "image0", 1: "image1"}
         image_set2 = {0: "image2", 1: "image3", 2: "image4"}
@@ -32,3 +37,4 @@ class ImageStackTests(unittest.TestCase):
         self.assertEqual(stack[2], "image2")
         self.assertEqual(stack[3], "image3")
         self.assertEqual(stack[4], "image4")
+
