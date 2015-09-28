0c66f65206f840dff92c20db0a3eb1eadac24fd7
jim <jim@rybarski.com>
2015-09-27 20:48:00 -0500

    more tests

--- tests/model/image_stack.py
+++ tests/model/image_stack.py
@@ -3,9 +3,9 @@ from model.image.stack import ImageStack
 
 
 class MockImage(object):
-    def __init__(self):
+    def __init__(self, field_of_view=0):
         self.data = "image_data"
-
+        self.field_of_view = field_of_view
 
 class ImageStackTests(unittest.TestCase):
     def test_add_correct_length(self):
@@ -43,3 +43,26 @@ class ImageStackTests(unittest.TestCase):
         self.assertEqual(stack[3], "image3")
         self.assertEqual(stack[4], "image4")
 
+    def test_iter(self):
+        stack = ImageStack()
+        image_set = {0: "image0", 1: "image1"}
+        image_set2 = {0: "image2", 1: "image3", 2: "image4"}
+        stack.add(image_set)
+        stack.add(image_set2)
+        images = [im for im in stack]
+        self.assertEqual(len(images), 5)
+
+    def test_filter(self):
+        stack = ImageStack()
+        image_set = {0: MockImage(field_of_view=0), 1: MockImage(field_of_view=1)}
+        image_set2 = {0: MockImage(field_of_view=0), 1: MockImage(field_of_view=1), 2: MockImage(field_of_view=3)}
+        stack.add(image_set)
+        stack.add(image_set2)
+        fov0 = [im for im in stack.filter(field_of_view=0)]
+        fov1 = [im for im in stack.filter(field_of_view=1)]
+        fov2 = [im for im in stack.filter(field_of_view=2)]
+        fov3 = [im for im in stack.filter(field_of_view=3)]
+        self.assertEqual(len(fov0), 2)
+        self.assertEqual(len(fov1), 2)
+        self.assertEqual(len(fov2), 0)
+        self.assertEqual(len(fov3), 1)
