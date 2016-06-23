74df790c56f4f4903e0cffcc68a4f71b0d96ea91
Jim Rybarski <jim@rybarski.com>
2016-06-23 15:09:24 -0500

    fixed backend

--- main.py
+++ main.py
@@ -7,4 +7,12 @@ hdf5_filename = '/home/jim/fylm3/test.h5'
 device = Device.original
 
 # create an aligned image stack
-preprocess.main(tif_directory, hdf5_filename, device)
+# preprocess.main(tif_directory, hdf5_filename, device)
+
+import h5py
+from skimage import io
+
+with h5py.File("/home/jim/fylm3/test.h5") as h5:
+    image = h5['/1/2/BF/0'].value
+    io.imshow(image)
+    io.show()
